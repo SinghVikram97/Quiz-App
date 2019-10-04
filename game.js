ff5c83af7fd6bfd7e282c9649d3bc4bfeda84be5
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-10-05 00:17:39 +0530

    Changed links

--- game.js
+++ game.js
@@ -52,7 +52,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     // GO TO END_PAGE
     localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
 
   questionCounter++;
